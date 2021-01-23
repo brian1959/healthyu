29f85e9c1d4c901c9b1741ab21dd9f436a553d7c
brian1959 <40475729+brian1959@users.noreply.github.com>
2021-01-23 15:14:10 -0700

    fixed data in pickyeaters to display cost

--- src/Components/Pickyeaters/pemealitem.js
+++ src/Components/Pickyeaters/pemealitem.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Checkbox } from "semantic-ui-react";
 
-export default function PeMealType (props){
 
+export default function PeMealType (props){
+const [checkitem, setCheckItem] = useState(false)
 
     return ( 
+
       <div className="aaMealType">
-<Checkbox onClick={() => this.handleMealType(props.mealtypeName, props.mealtypeCost)} />
+<Checkbox onClick= {() => {setCheckItem(true)},checkitem[props.mealtypeName, props.mealtypeCost]}/>
         <div className="aatopic">
-          <div className="aatitle">{props.mealtypeName}</div>
+          <div className="aatitle">{props.mealtypeName} - {props.mealtypeTcost}</div>
           <div className="aatext">{props.mealtypeBody}</div>
         </div>
       </div>
