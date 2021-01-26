e1e24c7b8adeaec2190eefc63b2af0cf8c3eb9da
brian1959 <40475729+brian1959@users.noreply.github.com>
2021-01-25 20:24:48 -0700

    fixing purchases functions

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Login extends Component {
-	login = () => {
+export default function Login(props) {
+	const login = () => {
 		let { REACT_APP_DOMAIN, REACT_APP_CLIENT_ID } = process.env;
 		let uri = `${encodeURIComponent(window.location.origin)}/auth/callback`;
 		window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${uri}&response_type=code`;
 	};
 
-	render() {
+	
 		return (
-			<div className="login-btn" onClick={this.login}>
-					Login
-							</div>
+
+			props.button ? (
+				<button className="login-button" onClick={login}>
+				Get Started
+						</button>	
+			):(
+				<div className="login-btn" onClick={login}>
+				Login
+						</div>
+			)
+
 		);
 	}
-}
+
