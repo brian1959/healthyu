873af7e5c59e5583bfcd3a1b96cbeb91dc348b1f
brian1959 <40475729+brian1959@users.noreply.github.com>
2021-02-13 16:18:47 -0700

    added footer with instagram link

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 
+
 export default function Footer(props) {
   return (
     <div className="footer-main">
       <div className="footer-form">
-        instagram
+       <a href="https://www.instagram.com/kandishealthyu2/">Follow us on instagram</a> 
         
       </div>
     </div>
