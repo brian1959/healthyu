4414a5ac730f2838ed490d8a0318431727d0c3e3
brian1959 <40475729+brian1959@users.noreply.github.com>
2021-02-20 16:31:22 -0700

    changed proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
-	app.use(proxy("/api", { target: "http://healthyu2.com:4035" }));
-	app.use(proxy("/auth", { target: "http://healthyu2.com:4035" }));
+	app.use(proxy("/api", { target: "http://localhost:4035" }));
+	app.use(proxy("/auth", { target: "http://localhost:4035" }));
 };
