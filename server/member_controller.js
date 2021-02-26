57090db09e14052f9f0b9987193b2531b3626f59
brian1959 <40475729+brian1959@users.noreply.github.com>
2021-02-25 22:43:36 -0700

    Revert "added 0 for memid option"
    This reverts commit 5d595fc4afe662ffe8a37793ccece95df023235a.

--- server/member_controller.js
+++ server/member_controller.js
@@ -2,7 +2,7 @@ module.exports = {
   
     getMember: async (req, res) => {
         const db = req.app.get("db");
-        const { memid=0 } = req.session.user;
+        const { memid } = req.session.user;
         let member = await db.get_member([memid]);
         res.status(200).send(member);
       },
